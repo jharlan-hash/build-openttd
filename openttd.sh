2569edc21a3ad8cd2e5ab604d016221286df3392
jharlan-hash <jharlancheese01@gmail.com>
2024-01-30 21:37:53 -0600

    fix openttd.sh

--- openttd.sh
+++ openttd.sh
@@ -2,6 +2,10 @@
 
 SCRIPT_DIR=$(dirname $0)
 
-cp -R $SCRIPT_DIR/../OpenTTD /Users/$USER/Documents/
+cd $SCRIPT_DIR
+
+cd ..
+
+cp -R ./OpenTTD /Users/$USER/Documents/
 /Users/$USER/Library/$1/OpenTTD/build/openttd
 rm -rf /Users/$USER/Documents/OpenTTD
